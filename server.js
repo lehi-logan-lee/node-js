24de4d1cf32b9e3932937a35a6983983df0fbb26
lehi-logan-lee <lihai0119@gmail.com>
2020-06-29 03:56:56 -0600

    parsed obj

--- server.js
+++ server.js
@@ -59,7 +59,8 @@ function getPerson(request, response) {
 		} else {
             const person = result[0];
             var my_obj = JSON.stringify(person);
-            var fir = my_obj.first;
+            var fir = JSON.parse(my_obj);
+            var firstName = fir.first;
 
             //person.toString()
             //const first = "SELECT first FROM person WHERE id = $1::int";
@@ -69,7 +70,7 @@ function getPerson(request, response) {
               //app.get('/getPerson', (request, response) => {
               //const weight = +req.body.weight
               //const id = request.body.id
-            const obj = { first: fir, id: id }
+            const obj = { first: firstName, id: id }
 
             response.render('pages/getPerson', obj)
               //})
