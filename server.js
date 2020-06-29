23299d97b7f65adc578187e04be1ff3f5b2759e2
lehi-logan-lee <lihai0119@gmail.com>
2020-06-28 18:36:41 -0600

    use MVC

--- server.js
+++ server.js
@@ -20,25 +20,25 @@ const pool = new Pool({connectionString: connectionString});
 app.set('port', (process.env.PORT || 5000));
 app.use(express.static(__dirname + '/public'));
 //I added it
-/*express()
-  .use(express.static(path.join(__dirname, 'public')))
-  .use(bodyParser.json())
-  .use(bodyParser.urlencoded({ extended: true }))
-  .set('views', path.join(__dirname, 'views'))
-  .set('view engine', 'ejs')
-  .get('/', (req, res) => res.render('pages/index'))
-  .post('/postage', (req, res) => {
-    const weight = +req.body.weight
-    const type = req.body.type
-    const obj = { weight: weight, type: type, result: calculateRate(weight, type) }
+//express()
+  //.use(express.static(path.join(__dirname, 'public')))
+  app.use(bodyParser.json())
+  app.use(bodyParser.urlencoded({ extended: true }))
+  app.set('views', path.join(__dirname, 'views'))
+  app.set('view engine', 'ejs')
+  app.get('/', (req, res) => res.render('pages/index'))
+  app.post('/getPerson', (req, res) => {
+    //const weight = +req.body.weight
+    const type = req.body.id
+    const obj = { type: type, result: getPerson(request, response) }
 
-    res.render('pages/postage', obj)
+    res.render('pages/getPerson', obj)
   })
-  .listen(PORT, () => console.log(`Listening on ${ PORT }`))*/
+  //.listen(PORT, () => console.log(`Listening on ${ PORT }`))*/
 
 // This says that we want the function "getPerson" below to handle
 // any requests that come to the /getPerson endpoint
-app.get('/getPerson', getPerson);
+//app.get('/getPerson', getPerson);
 
 // Start the server running
 app.listen(app.get('port'), function() {
